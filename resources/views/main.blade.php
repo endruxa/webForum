443e8eedc13536fd9c2c81d865d0126076da2f59
endruxa <nesterov.andrey.a@gmail.com>
2018-09-24 00:54:04 +0300

    threads

---
+++ resources/views/main.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.front')
+@section('content')
+
+    <div class="jumbotron">
+        <h1 class="display-5">Join Comunity</h1>
+        <p class="lead">Help and get Help</p>
+        <hr class="my-4">
+        <p class="lead">
+            <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+        </p>
+    </div>
+
+@endsection
